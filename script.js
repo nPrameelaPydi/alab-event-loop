1234f9abd54a76e899052985e28052d10a33be28
Prameela Pydi <nprameela.pydi@gmail.com>
2024-10-05 19:31:36 -0700

    Part 1: Stack Overflow

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log(`Alab - Practical use of event loop`);
+
+//Part 1: Stack Overflow
+let count = 0;
+function incrementCounter() {
+    count++;
+    console.log(`Counter value: ${count}`);
+    incrementCounter(); //recursive call
+}
+//incrementCounter()//func call to start recursion
+
+// Surround the initial function call in a try/catch block
+try {
+    incrementCounter();
+} catch (error) {
+    console.log("Error message: ", error.message)
+}
+
+//The error.message property is part of the standard error object in JavaScript
+//when an exception is thrown, js creates an error object, it has several properties, one of which is message
