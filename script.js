9f6adb7b311614fd70835c4e5d9be50b97738a92
Prameela Pydi <nprameela.pydi@gmail.com>
2024-10-06 10:10:41 -0700

    modifird script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function incrementCounter() {
     incrementCounter(); //recursive call
 }
 //incrementCounter()//func call to start recursion
-// Surround the initial function call in a try/catch block
+// surround initial func call in a try/catch block
 try {
     incrementCounter();
 } catch (error) {
@@ -21,23 +21,23 @@ try {
 //#########Part 2: Trampolines
 //trampolining - eliminates stack overflow issue if implemented correctly.
 const factorial = (n) => {
-    if (n === 0) return 1; // The base case, to stop recursion
-    return n * factorial(n - 1); // The recursive call
+    if (n === 0) return 1; // base case to stop recursion
+    return n * factorial(n - 1); //recursive call
 }
 //console.log(factorial(5));
 function flatten(arr) {
     function _flatten(arr, result) {
         if (arr.length === 0) return result;
-        const head = arr[0]; // Get the first element
-        const tail = arr.slice(1); // Get the rest of the array
+        const head = arr[0]; //first element in the array
+        const tail = arr.slice(1); //rest of the array
 
         if (Array.isArray(head)) {
-            // If head is an array, concatenate it with tail
+            //if head is an array, concatenate it with tail
             return function () {
                 return _flatten(head.concat(tail), result);
             };
         } else {
-            // If head is not an array, add it to the result
+            //if head is not an array, add it to the result
             return function () {
                 return _flatten(tail, result.concat(head));
             };
@@ -48,7 +48,7 @@ function flatten(arr) {
 function trampoline(fn) {
     let result = fn;
     while (typeof result === 'function') {
-        result = result(); // Execute the returned function
+        result = result(); //execute the returned function
     }
     return result;
 }
